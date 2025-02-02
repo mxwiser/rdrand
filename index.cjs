a4f1374666df7b4e002121b10e52b64bbaeeca18
mxwiser <neoway@126.com>
2025-02-03 02:03:42 +0800

    fix bugs

--- index.cjs
+++ index.cjs
@@ -1,22 +1,13 @@
-const ffi = require('ffi-napi');
+const koffi = require('koffi');
+const path = require('path');
+const libPath = path.resolve(__dirname, 'librdrand.so'); 
+const lib = koffi.load(libPath);
 
-
-
-
-const mylib = ffi.Library('./librdrand', {
-    'is_rdrand_supported': ['int', []],
-    'rdrand32': ['uint32', []],
-    'rdrand64': ['uint64', []],
-    'rdseed32': ['uint32', []],
-    'rdseed64': ['uint64', []],
-})
-
-
-module.exports.isRrdrandSupported= mylib.is_rdrand_supported;
-module.exports.rdRand32= mylib.rdrand32;
-module.exports.rdRand64= mylib.rdrand64;
-module.exports.rdSeed32= mylib.rdseed32;
-module.exports.rdSeed64= mylib.rdseed64;
+module.exports.isRrdrandSupported= lib.func('int is_rdrand_supported()');
+module.exports.rdRand32= lib.func('uint32 rdrand32()')
+module.exports.rdRand64= lib.func('uint64 rdrand64()')
+module.exports.rdSeed32= lib.func('uint32 rdseed32()')
+module.exports.rdSeed64= lib.func('uint64 rdseed64()')
 module.exports.normalizeUint32=(uint32)=> {
     return  uint32 / 0xFFFFFFFF;
 }

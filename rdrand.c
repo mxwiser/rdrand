3e32c2d217c006ce0cd84c7f39ce8314f7879a38
mxwiser <neoway@126.com>
2025-02-04 08:53:22 +0800

    update

--- rdrand.c
+++ rdrand.c
@@ -1,64 +1,62 @@
 #include <stdio.h>
 #include <stdint.h>
 
-//build: gcc -shared  -o librdrand.so rdrand.c
-int is_rdrand_supported() {
+// build: gcc -shared  -o librdrand.so rdrand.c
+int is_rdrand_supported()
+{
     uint32_t eax, ebx, ecx, edx;
     eax = 1;
-    __asm__ volatile (
+    __asm__ volatile(
         "cpuid"
-        : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx)
-        : "a" (eax)
-    );
+        : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
+        : "a"(eax));
     return (ecx >> 30) & 1;
 }
 
-int rdrand32() {
+int rdrand32()
+{
     uint32_t rand;
     unsigned char ok;
-    __asm__ volatile (
+    __asm__ volatile(
         "rdrand %%eax; setc %0"
-        : "=r" (ok), "=a" (rand)
+        : "=r"(ok), "=a"(rand)
         :
-        : "cc"
-    );
+        : "cc");
     return (uint32_t)rand;
 }
 
-int rdseed32() {
+int rdseed32()
+{
     uint32_t rand;
     unsigned char ok;
-    __asm__ volatile (
+    __asm__ volatile(
         "rdseed %%eax; setc %0"
-        : "=r" (ok), "=a" (rand)
+        : "=r"(ok), "=a"(rand)
         :
-        : "cc"
-    );
+        : "cc");
     return (uint32_t)rand;
 }
 
-
-
-int rdrand64() {
+int rdrand64()
+{
     uint64_t rand;
     unsigned char ok;
-    __asm__ volatile (
+    __asm__ volatile(
         "rdrand %%rax; setc %0"
-        : "=r" (ok), "=a" (rand)
+        : "=r"(ok), "=a"(rand)
         :
-        : "cc"
-    );
+        : "cc");
     return (uint64_t)rand;
 }
 
-int rdseed64() {
+int rdseed64()
+{
     uint64_t rand;
     unsigned char ok;
-    __asm__ volatile (
+    __asm__ volatile(
         "rdseed %%rax; setc %0"
-        : "=r" (ok), "=a" (rand)
+        : "=r"(ok), "=a"(rand)
         :
-        : "cc"
-    );
+        : "cc");
     return (uint64_t)rand;
 }
